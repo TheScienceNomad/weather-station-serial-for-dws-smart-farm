a53febb4d0ca5ac2604065f7880deac8e817bae7
TheScienceNomad <68929873+TheScienceNomad@users.noreply.github.com>
2023-06-09 17:11:08 +1000

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://thesciencenomad.github.io/weather-station-serial-for-dws-smart-farm/](https://thesciencenomad.github.io/weather-station-serial-for-dws-smart-farm/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/thesciencenomad/weather-station-serial-for-dws-smart-farm** and import
+
+## Edit this project ![Build status badge](https://github.com/thesciencenomad/weather-station-serial-for-dws-smart-farm/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/thesciencenomad/weather-station-serial-for-dws-smart-farm** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/thesciencenomad/weather-station-serial-for-dws-smart-farm/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,58 @@
+function UpdateWindDirection () {
+    Wind_Direction = parseFloat(Raw_Data.substr(7, 3))
+}
+function UpateTemp () {
+    Temp = parseFloat(Raw_Data.substr(56, 3))
+}
+function UpdateWeatherData () {
+    UpateTemp()
+    UpdateWindDirection()
+    UpdateHumidity()
+    UpdateRainFall()
+    UpdateWindSpeed()
+}
+bluetooth.onBluetoothConnected(function () {
+    isconnected = 1
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.clearScreen()
+    serial.redirect(
+    SerialPin.P13,
+    SerialPin.P14,
+    BaudRate.BaudRate9600
+    )
+    serial.setRxBufferSize(128)
+    serial.setTxBufferSize(128)
+})
+function UpdateHumidity () {
+    Humidity = Raw_Data.substr(61, 3)
+}
+function UpdateRainFall () {
+    Rain_Total = parseFloat(Raw_Data.substr(56, 3))
+}
+function UpdateWindSpeed () {
+    Wind_Speed = parseFloat(Raw_Data.substr(56, 3))
+}
+serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () {
+    Raw_Data = serial.readUntil(serial.delimiters(Delimiters.CarriageReturn))
+    UpdateWeatherData()
+})
+let Wind_Speed = 0
+let Rain_Total = 0
+let Humidity = ""
+let Temp = 0
+let Raw_Data = ""
+let Wind_Direction = 0
+let isconnected = 0
+isconnected = 0
+basic.showIcon(IconNames.SmallHeart)
+basic.pause(2000)
+bluetooth.startUartService()
+loops.everyInterval(2000, function () {
+    if (isconnected == 1) {
+        bluetooth.uartWriteLine("" + Humidity + "," + Temp + "," + Wind_Direction + "," + Wind_Speed + "," + Rain_Total)
+    }
+})
+basic.forever(function () {
+	
+})
